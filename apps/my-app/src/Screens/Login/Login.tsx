3ab38f5e9c1fb427a8e4cf9813287c004fbb200d
Andrew Pynch <andrewpynchbusiness@gmail.com>
2022-10-01 16:01:01 -0500

    gogoogo

---
+++ apps/my-app/src/Screens/Login/Login.tsx
@@ -0,0 +1,17 @@
+type LoginProps = {};
+
+const Login = (props: LoginProps) => {
+    return (
+        <div
+            style={{
+                width: '500px',
+                height: '500px',
+                backgroundColor: 'red',
+            }}
+        >
+            Login
+        </div>
+    );
+};
+
+export default Login;

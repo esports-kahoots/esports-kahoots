ebcd5c1e040b000fdc9666a4d69ab513414b5c25
Andrew Pynch <andrewpynchbusiness@gmail.com>
2022-10-02 10:19:29 -0500

    fix use custom theme shit

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,27 +1,133 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { NativeBaseProvider, extendTheme } from "native-base";
-import reportWebVitals from "./reportWebVitals";
+import { ColorMode, extendTheme, NativeBaseProvider } from 'native-base';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-  },
+const customTheme = extendTheme({
+    config: {
+        initialColorMode: 'dark',
+    },
+    colors: {
+        text: {
+            primary: {
+                light: '#060606',
+                dark: '#E9E9E9',
+            },
+            secondary: {
+                light: '#221E1E',
+                dark: '#aaaaaa',
+            },
+            grey: {
+                primary: {
+                    light: '#BFB9B9',
+                    dark: '#aaaaaa',
+                },
+                secondary: {
+                    light: '#514E4E',
+                    dark: '#E9E9E9',
+                },
+            },
+            alternate: {
+                light: '#706B6B',
+                dark: '#706B6B',
+            },
+            speaking: {
+                light: '#A29E9E',
+                dark: '#A29E9E',
+            },
+        },
+        icon: {
+            primary: {
+                light: '#514E4E',
+                dark: '#F6F0E7',
+            },
+        },
+        background: {
+            primary: {
+                light: '#FEFAE0',
+                dark: '#353232',
+            },
+            secondary: {
+                light: '#FEFAD0',
+                dark: '#221E1E',
+            },
+            tertiary: {
+                light: '#FCF6EE',
+                dark: '#403E3E',
+            },
+            hover: {
+                light: '#FCF6EE',
+                dark: '#403E3E',
+            },
+            options: {
+                light: '#FCF6EE',
+                dark: '#221E1E',
+            },
+            medBeige: {
+                light: '#FCF6EE',
+                dark: '#403E3E',
+            },
+            beige: {
+                light: '#F7F0E7',
+                dark: '#403E3E',
+            },
+        },
+        primary: {
+            light: '#DDA15E',
+            dark: '#DDA15E',
+        },
+        primaryAccent: {
+            light: '#BC6C25',
+            dark: '#BC6C25',
+        },
+        secondary: {
+            light: '#606C38',
+            dark: '#606C38',
+        },
+        secondaryHover: {
+            light: '#283618',
+            dark: '#283618',
+        },
+    },
 });
 
 // extend the theme
-type MyThemeType = typeof theme;
-declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
+type MyThemeType = typeof customTheme;
+declare module 'native-base' {
+    interface ICustomTheme extends MyThemeType {}
 }
+
+const config = {
+    // dependencies: {
+    //     'linear-gradient': require('react-native-web-linear-gradient').default,
+    // },
+};
+
+// Define the native base color mode manager that will persist the dark mode setting
+const colorModeManager: any = {
+    get: async () => {
+        let val = localStorage.getItem('dark-mode');
+        return val === 'dark' ? 'dark' : 'light';
+    },
+    set: async (value: ColorMode) => {
+        let strValue = value ? value.toString() : '';
+        localStorage.setItem('dark-mode', strValue);
+    },
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <NativeBaseProvider theme={theme}>
-      <App />
-    </NativeBaseProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <NativeBaseProvider
+            colorModeManager={colorModeManager}
+            theme={customTheme}
+            config={config}
+        >
+            <App />
+        </NativeBaseProvider>
+        ,
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ apps/web/src/useCustomTheme.ts
@@ -0,0 +1,107 @@
+import { useColorModeValue, useTheme } from 'native-base';
+
+export const useCustomTheme = () => {
+    const { colors } = useTheme();
+
+    // // Text
+    const textColor = useColorModeValue(
+        colors.text.primary.light,
+        colors.text.primary.dark
+    );
+
+    const textColorSecondary = useColorModeValue(
+        colors.text.secondary.light,
+        colors.text.secondary.dark
+    );
+
+    const textColorLight = useColorModeValue(
+        colors.text.primary.dark,
+        colors.text.primary.light
+    );
+
+    const textColorGrey = useColorModeValue(
+        colors.text.grey.primary.light,
+        colors.text.grey.primary.dark
+    );
+
+    const textColorGreySecondary = useColorModeValue(
+        colors.text.grey.secondary.light,
+        colors.text.grey.secondary.dark
+    );
+
+    const textColorAlternate = useColorModeValue(
+        colors.text.alternate.light,
+        colors.text.alternate.dark
+    );
+
+    const textColorSpeaking = useColorModeValue(
+        colors.text.speaking.light,
+        colors.text.speaking.dark
+    );
+
+    // Backgrounds
+    const backgroundColor = useColorModeValue(
+        colors.background.primary.light,
+        colors.background.primary.dark
+    );
+
+    const backgroundColorSecondary = useColorModeValue(
+        colors.background.secondary.light,
+        colors.background.secondary.dark
+    );
+
+    const backgroundColorTertiary = useColorModeValue(
+        colors.background.tertiary.light,
+        colors.background.tertiary.dark
+    );
+
+    const backgroundMedBeige = useColorModeValue(
+        colors.background.medBeige.light,
+        colors.background.medBeige.dark
+    );
+
+    const backgroundBeige = useColorModeValue(
+        colors.background.beige.light,
+        colors.background.beige.dark
+    );
+
+    const backgroundColorHover = useColorModeValue(
+        colors.background.hover.light,
+        colors.background.hover.dark
+    );
+
+    const backgroundColorOptions = useColorModeValue(
+        colors.background.options.light,
+        colors.background.options.dark
+    );
+
+    // Icons
+    const iconTextColor = useColorModeValue(
+        colors.icon.primary.light,
+        colors.icon.primary.dark
+    );
+
+    // Transition speeds
+    const transitionSpeed = 150;
+    const transitionSpeedLayout = 250;
+
+    return {
+        textColor,
+        textColorSecondary,
+        textColorLight,
+        textColorGrey,
+        textColorGreySecondary,
+        textColorAlternate,
+        textColorSpeaking,
+        backgroundColor,
+        backgroundColorSecondary,
+        backgroundColorTertiary,
+        backgroundMedBeige,
+        backgroundColorHover,
+        backgroundColorOptions,
+        backgroundBeige,
+        iconTextColor,
+        transitionSpeed,
+        transitionSpeedLayout,
+    };
+};

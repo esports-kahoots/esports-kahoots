1a51e972ea54d096471790ebf60fd77deeac3e0f
Andrew Pynch <andrewpynchbusiness@gmail.com>
2022-10-02 15:19:17 -0500

    login screen styles'

--- apps/web/src/Screens/Login/Login.tsx
+++ apps/web/src/Screens/Login/Login.tsx
@@ -1,15 +1,93 @@
+import { Button, HStack, VStack } from 'native-base';
+import GoogleIcon from '../../Assets/Icons/GoogleIcon';
+import { HorizontalDivider } from '../../Components/Themed/Grid';
+import { TText } from '../../Components/Themed/Text';
+import { useCustomTheme } from '../../useCustomTheme';
+
 type LoginProps = {};
 
 const Login = (props: LoginProps) => {
+    const {
+        textColorLight,
+        backgroundColor,
+        primary,
+        primaryAccent,
+        secondary,
+        secondaryAccent,
+    } = useCustomTheme();
+
     return (
         <div
             style={{
-                width: '500px',
-                height: '500px',
-                backgroundColor: 'red',
+                backgroundColor: backgroundColor,
+                height: '100vh',
+                width: '100vw',
+                flexDirection: 'row',
+                display: 'flex',
             }}
         >
-            Login
+            <VStack width="33.3%" />
+            <VStack alignItems="center" flexDir="column" width="33.3%">
+                <HStack
+                    flexDir="row"
+                    alignItems="center"
+                    height="100%"
+                    width="100%"
+                >
+                    <VStack alignItems="center" width="100%">
+                        <Button
+                            width="100%"
+                            leftIcon={<GoogleIcon />}
+                            backgroundColor={primary}
+                            _hover={{
+                                backgroundColor: primaryAccent,
+                            }}
+                        >
+                            <TText
+                                style={{
+                                    fontWeight: 700,
+                                    fontSize: '18px',
+                                    color: backgroundColor,
+                                }}
+                            >
+                                Continue As Guest
+                            </TText>
+                        </Button>
+
+                        <TText
+                            style={{
+                                fontWeight: 600,
+                                fontSize: '12px',
+                                color: secondary,
+                                marginTop: '1em',
+                                marginBottom: '-2em',
+                            }}
+                        >
+                            or...
+                        </TText>
+                        <HorizontalDivider width="120%" color={secondary} />
+
+                        <Button
+                            width="100%"
+                            backgroundColor={primaryAccent}
+                            _hover={{
+                                backgroundColor: primary,
+                            }}
+                        >
+                            <TText
+                                style={{
+                                    fontWeight: 700,
+                                    fontSize: '18px',
+                                    color: backgroundColor,
+                                }}
+                            >
+                                Continue As Guest
+                            </TText>
+                        </Button>
+                    </VStack>
+                </HStack>
+            </VStack>
+            <VStack width="33.3%" />
         </div>
     );
 };

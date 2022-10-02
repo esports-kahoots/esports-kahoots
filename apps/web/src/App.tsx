2f84b9aec65a38beeab07ae493f6a158974f953a
kameron.hnath <kameron.hnath@gmail.com>
2022-10-02 16:28:10 -0400

    Fixed some problems with CodeScreen

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/profile" element={<Profile />} />
-                <Route path="/codescreen" element={<CodeScreen />} />
+                <Route path="/codescreen" element={<CodeScreen code={"ABCDEF"} />} />
             </Routes>
         </Router>
     );
--- apps/web/src/Screens/CodeScreen/CodeScreen.tsx
+++ apps/web/src/Screens/CodeScreen/CodeScreen.tsx
@@ -1,28 +1,32 @@
-import { Center, Text} from 'native-base';
-import { useCustomTheme } from './../../useCustomTheme';
+import { Center} from "native-base";
+import { TText } from "../../Components/Themed/Text";
+import { useCustomTheme } from "./../../useCustomTheme";
 
-type CodeScreenProps = {};
+type CodeScreenProps = {
+    code: string
+};
 
 const CodeScreen = (props: CodeScreenProps) => {
-    const { backgroundColor } = useCustomTheme();
+  const { backgroundColor, secondaryAccent, primary } = useCustomTheme();
 
-
-
-
-    return (
-
-
-        <Center width="100vw" height="100vh" alignItems="center" backgroundColor={backgroundColor}>
-            <Center width="85vw" height="85vh"
-            alignSelf="center"
-            justifyContent="center"
-            background="#DDA15E"
-            opacity="0.7"
-            backdrop-filter="blur(2px)">
-                <Text fontSize="240">A</Text>
-            </Center>
-        </Center>
-    );
+  return (
+    <Center
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      backgroundColor={backgroundColor}
+    >
+      <Center
+        width="85vw"
+        height="85vh"
+        background={primary}
+        opacity="0.7"
+        backdrop-filter="blur(2px)"
+      >
+        <TText style={{fontSize: '280px', fontWeight: 'bolder', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', color: secondaryAccent}}>{props.code}</TText>
+      </Center>
+    </Center>
+  );
 };
 
 export default CodeScreen;

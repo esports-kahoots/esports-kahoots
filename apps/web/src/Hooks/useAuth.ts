d22f042e5ac811c967e84cfabc83a020451f6483
Andrew Pynch <andrewpynchbusiness@gmail.com>
2022-10-02 22:13:06 -0500

    pysg for the homies

---
+++ apps/web/src/Hooks/useAuth.ts
@@ -0,0 +1,8 @@
+import { FirebaseApp } from 'firebase/app';
+
+// 1. Connect the app the emulators
+export let app: FirebaseApp;
+
+// 2. create some auth hooks for signing in / getting firebase user
+
+// 3. ?? misc ??

1435a015134331ce1d0bb791f75e8a1cba6e682e
Andrew Pynch <andrewpynchbusiness@gmail.com>
2022-10-11 21:02:28 -0500

    PUSH

--- apps/middleware/functions/src/index.ts
+++ apps/middleware/functions/src/index.ts
@@ -1,31 +1,40 @@
 //import libraries
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
-import * as bodyParser from "body-parser";
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
-export let app: express.Express
-export let webApi: any
-export const fs = admin.firestore();
+export let app: express.Express;
+export let webApi: any;
+
+const projectConfig = {
+    projectId: 'esports-kahoots',
+    storageBucket: 'esports-kahoots.appspot.com',
+    databaseURL: 'http://localhost:9001/?ns=esports-kahoots-default-rtdb',
+};
+//initialize firebase inorder to access its services
+admin.initializeApp(projectConfig);
 
 try {
-    //initialize firebase inorder to access its services
-    admin.initializeApp(functions.config().firebase);
-
     //initialize express server
     app = express();
     const main = express();
 
-    //add the path to receive request and set json as bodyParser to process the body 
+    //add the path to receive request and set json as bodyParser to process the body
     main.use('/api/v1', app);
     main.use(bodyParser.json());
     main.use(bodyParser.urlencoded({ extended: false }));
 
     //define google cloud function name
     webApi = functions.https.onRequest(main);
-}
-catch (e) {
-    console.error("!!", e)
+
+    app.get('/', async (req, res) => {
+        res.status(200).send('HELLO FROM FIREBASE');
+    });
+} catch (e) {
+    console.error('!!', e);
 }
 
-export * from "./testapi"
+export const fs = admin.firestore();
+
+export * from './testapi';
